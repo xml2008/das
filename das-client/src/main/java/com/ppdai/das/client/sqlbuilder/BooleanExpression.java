bd5e97abcdc9195d5146f21f25b4621b03bd729c
hejiehui <he_jiehui@163.com>
2019-12-11 12:27:27 +0800

    fix SqlBuilder.includeAll() condition calculation
    (cherry picked from commit 4a083eb)

--- das-client/src/main/java/com/ppdai/das/client/sqlbuilder/BooleanExpression.java
+++ das-client/src/main/java/com/ppdai/das/client/sqlbuilder/BooleanExpression.java
@@ -1,6 +1,11 @@
 package com.ppdai.das.client.sqlbuilder;
 
-public class BooleanExpression extends Expression {
+import com.ppdai.das.strategy.ColumnCondition;
+import com.ppdai.das.strategy.Condition;
+import com.ppdai.das.strategy.ConditionProvider;
+import com.ppdai.das.strategy.OperatorEnum;
+
+public class BooleanExpression extends Expression implements ConditionProvider {
     private String template;
     
     public static final BooleanExpression TRUE = new BooleanExpression("TRUE");
@@ -22,5 +27,10 @@ public class BooleanExpression extends Expression {
     @Override
     public String build(BuilderContext helper) {
         return template;
+    }
+
+    @Override
+    public Condition build() {
+        return new ColumnCondition(OperatorEnum.UN_DEFINED, "", "", null);
     }
 }
