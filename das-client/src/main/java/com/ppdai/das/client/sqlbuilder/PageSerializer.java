95544a247bb9d07e5a644330ce630dc4833a193d
ShengyuanLu <sting_eleven@hotmail.com>
2020-06-28 15:15:50 +0800

    PageSerializer.deserialize

--- das-client/src/main/java/com/ppdai/das/client/sqlbuilder/PageSerializer.java
+++ das-client/src/main/java/com/ppdai/das/client/sqlbuilder/PageSerializer.java
@@ -7,12 +7,16 @@ import com.ppdai.das.client.Segment;
 public class PageSerializer implements Serializer {
     @Override
     public Segment deserialize(JsonObject jsonObject) {
-        return new Page(0,0);
+        int pageNo = Integer.parseInt(jsonObject.get("pageNo").getAsString());
+        int pageSize = Integer.parseInt(jsonObject.get("pageSize").getAsString());
+        return new Page(pageNo, pageSize);
     }
 
     @Override
     public JsonObject serialize(Segment segment) {
         JsonObject element = new JsonObject();
+        element.addProperty("pageNo", readField(segment, "pageNo").toString());
+        element.addProperty("pageSize", readField(segment, "pageSize").toString());
         return addBuildType(element);
     }
 
